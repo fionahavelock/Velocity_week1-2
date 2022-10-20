e9a5e00ce2f82e2889a66677c22c4faac1841393
Fiona Havelock <fiona.havelock@avanade.com>
2022-10-20 15:14:39 +0100

    git commit

--- conditionals.py
+++ conditionals.py
@@ -1,6 +1,8 @@
 
 mark = int(input("Please enter your mark: "))
-if mark > 65:
-    print("You have passed")
-else:
+if mark <= 50:
     print("You have failed")
+elif mark > 50 and mark <= 69:
+    print("You have a merit")
+else:
+    print("You have a distiction")
