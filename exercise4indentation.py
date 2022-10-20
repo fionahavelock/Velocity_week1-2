7dd37697f8520f5e13529ec0b198a039be385ca0
Fiona Havelock <fiona.havelock@avanade.com>
2022-10-20 10:25:03 +0100

    Python code commit

---
+++ exercise4indentation.py
@@ -0,0 +1,8 @@
+temperature = 25
+if temperature >= 40:
+    print("too hot")
+    print("arghh")
+elif temperature >= 20 and temperature < 40:
+    print("just right")
+else:
+    print("too cold")
